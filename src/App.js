3f9e6385d473be83f17ac2e4d6d034b6ffa70d19
DuaaH <dodohalaiqa.1@gmail.com>
2020-08-19 15:30:29 +0300

    add routing with React Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Game from './pages/Game';
+import HighScores from './pages/HighScores';
+import GameOver from './pages/gameOver';
+import Home from './pages/Home';
 import './App.css';
 
 function App() {
   return (
-    <div >
-      App
-    </div>
+    <Router >
+      <Switch>
+        <Route path="/game" component={Game} />
+        <Route path="/highScores" component={HighScores} />
+        <Route path="/gameOver" component={GameOver} />
+        <Route path="/" component={Home} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/pages/HighScores.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function HighScores() {
+  return (
+    <div>
+      <h1>
+        HighScores
+      </h1>
+    </div>
+  )
+}
