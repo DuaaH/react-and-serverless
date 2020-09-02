1201526fe89ee7ba0fdc0695bd7a3f462ef17c77
DuaaH <dodohalaiqa.1@gmail.com>
2020-09-02 15:26:46 +0300

    style the Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import CTA from '../styled/CTA'
+import { Accent, StyledTitle } from '../styled/Random'
 
 export default function Home() {
   return (
     <div>
-      <h1>
-        home
-      </h1>
+      <StyledTitle>
+        Ready to type?!
+      </StyledTitle>
+      <CTA to='/game'>
+        Click or type <Accent>'s'</Accent> to start playing!
+      </CTA>
     </div>
   )
 }
