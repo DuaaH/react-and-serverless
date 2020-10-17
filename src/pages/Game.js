fa2a31ace2104eb78f29babd56c1c5aa97932f6d
DuaaH <dodohalaiqa.1@gmail.com>
2020-10-17 14:59:57 +0300

    change the chareacters

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import { StyledGame, StyledScore, StyledTimer, StyledCharacter } from '../styled/Game'
 import { Strong } from '../styled/Random'
 
@@ -7,12 +7,20 @@ export default function Game({ history }) {
   const MAX_SECONDS = 90;
   const [ms, setMs] = useState(0);
   const [seconds, setSeconds] = useState(MAX_SECONDS);
+  const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  const [currentCharacter, setCurrentCharacter] = useState('');
 
   useEffect(() => {
+    setRandomCharacter();
     const currentTime = new Date();
     const interval = setInterval(()=> updateTime(currentTime, 1))
     return ()=> clearInterval(interval)
   }, []);
+
+  const setRandomCharacter = () => {
+    const randomInt = Math.floor(Math.random() * 36);
+    setCurrentCharacter(characters[randomInt]);
+  };
 
 
   const updateTime= (startTime)=>{
@@ -42,22 +50,34 @@ export default function Game({ history }) {
     }
   }, [seconds, ms, history]);
 
-  const keyUpHandler = (e) => {
-    console.log(e.key);
-  };
+  const keyUpHandler = useCallback(
+    (e) => {
+      console.log(e.key, currentCharacter);
+      if (e.key === currentCharacter) {
+        setScore((prevScore) => prevScore + 1);
+      } else {
+        if (score > 0) {
+          setScore((prevScore) => prevScore - 1);
+        }
+      }
+      setRandomCharacter();
+    },
+    [currentCharacter]
+  );
+
 
   useEffect(() => {
     document.addEventListener('keyup', keyUpHandler);
     return () => {
       document.removeEventListener('keyup', keyUpHandler);
     };
-  }, []);
+  }, [keyUpHandler]);
 
 
   return (
     <StyledGame>
       <StyledScore>Score:<Strong>{score}</Strong></StyledScore>
-      <StyledCharacter>A</StyledCharacter>
+      <StyledCharacter>{currentCharacter}</StyledCharacter>
   <StyledTimer>Time: <Strong>{seconds} :{ms}</Strong></StyledTimer>
     </StyledGame>
   )
