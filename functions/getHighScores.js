60675705a887023d3b3354b283ebdcb4ba3c41a9
DuaaH <dodohalaiqa.1@gmail.com>
2020-11-03 14:53:15 +0200

    read the data from airtable and using the env

---
+++ functions/getHighScores.js
@@ -0,0 +1,33 @@
+const Airtable = require('airtable');
+require('dotenv').config();
+
+const { apiKey, BASE_ID , TABLE} = process.env;
+
+Airtable.configure({
+  apiKey: apiKey,
+});
+const base = Airtable.base(BASE_ID);
+const table = base.table(TABLE);
+
+exports.handler = async (event) => {
+  try {
+    const records = await table.select({}).firstPage();
+    const formattedRecords = records.map((record) => ({
+      id: record.id,
+      fields: record.fields,
+    }));
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(
+        formattedRecords
+      ),
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ err: 'Failed to get records from Airtable', }),
+    };
+  }
+
+};
