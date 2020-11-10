081aa39418df5a4df5665abcdeac8639215ae6b9
DuaaH <dodohalaiqa.1@gmail.com>
2020-11-10 16:38:23 +0200

    added style for buttons

---
+++ src/styled/Buttons.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const StyledButton = styled.button`
+    background-color: var(--main-text-color);
+    color: var(--main-bg-color);
+    cursor: pointer;
+    font-size: 1rem;
+    padding: 0.2rem 0.5rem;
+    border-radius: 5px;
+`;
+
+export const StyledButtonLink = styled.button`
+    border: none;
+    font-size: 1.2rem;
+    cursor: pointer;
+    background: none;
+      &:hover {
+        color: var(--accent-color);
+    }
+`;
