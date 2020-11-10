dfe32bb886f16ece0659d6906addf54f095d51a7
DuaaH <dodohalaiqa.1@gmail.com>
2020-11-10 15:24:29 +0200

    save theme in local storage

--- src/hooks/UseTheme.js
+++ src/hooks/UseTheme.js
@@ -1,14 +1,22 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 
 export default () => {
   const [theme, setTheme] = useState('light');
 
+  useEffect(() => {
+    const lsTheme = localStorage.getItem('theme');
+    setTheme(lsTheme || 'light');
+  }, []);
+
+
   const toggleTheme = () => {
     if (theme === 'light') {
       setTheme('dark');
+      localStorage.setItem('theme', 'dark');
     } else {
       setTheme('light');
+      localStorage.setItem('theme', 'light');
     }
   };
 
